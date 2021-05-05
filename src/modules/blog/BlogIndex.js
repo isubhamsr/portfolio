b0e5ea0cc22edced1f55ee4e4ac2e1af5168e2f5
Subham Roy <52313227+isubhamsr@users.noreply.github.com>
2021-05-05 20:49:50 +0530

    add blog post temp and add footer

--- src/modules/blog/BlogIndex.js
+++ src/modules/blog/BlogIndex.js
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 // import tinymce from 'tinymce/tinymce';
 import '@tinymce/tinymce-react'
-import { Editor } from '@tinymce/tinymce-react';
+import BlogPost from './Components/BlogPost';
 
 export default function BlogIndex() {
   const editorRef = useRef(null);
@@ -11,27 +11,8 @@ export default function BlogIndex() {
     }
   };
   return (
-    <div className="relative mx-4 my-4">
-      <Editor
-        onInit={(evt, editor) => editorRef.current = editor}
-        apiKey="42w1wak35v1aannpa8a8o5camazvi8cbr22t3zm8lmnw5bbu"
-        initialValue="<p>This is the initial content of the editor.</p>"
-        init={{
-          height: 500,
-          // menubar: 'insert',
-          plugins: [
-            'advlist autolink lists link image charmap print preview anchor',
-            'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount'
-          ],
-          toolbar: 'undo redo | formatselect | ' +
-            'bold italic underline backcolor | alignleft aligncenter ' +
-            'alignright alignjustify | bullist numlist outdent indent | ' +
-            'removeformat | help | image | code',
-          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
-        }}
-      />
-      <button onClick={log}>Log editor content</button>
-    </div>
+    <React.Fragment>
+      <BlogPost />
+    </React.Fragment>
   );
 }
--- src/modules/core/Base.js
+++ src/modules/core/Base.js
@@ -104,20 +104,14 @@ export default function Base({ children }) {
             </div>
             <div class="lg:w-1/4 md:w-1/2 w-full px-4">
               <h2 class="title-font font-medium text-gray-900 tracking-widest text-sm mb-3">
-                CATEGORIES
+              LEGAL
               </h2>
               <nav class="list-none mb-10">
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">First Link</a>
+                  <a class="text-gray-600 hover:text-gray-800">Privacy</a>
                 </li>
                 <li>
-                  <a class="text-gray-600 hover:text-gray-800">Second Link</a>
-                </li>
-                <li>
-                  <a class="text-gray-600 hover:text-gray-800">Third Link</a>
-                </li>
-                <li>
-                  <a class="text-gray-600 hover:text-gray-800">Fourth Link</a>
+                  <a class="text-gray-600 hover:text-gray-800">Terms</a>
                 </li>
               </nav>
             </div>
@@ -159,7 +153,7 @@ export default function Base({ children }) {
               <span class="ml-3 text-xl">Subham</span>
             </a>
             <p class="text-sm text-gray-500 sm:ml-6 sm:mt-0 mt-4">
-              © 2020 —
+              © 2020 — All rights reserved
               <a
                 href="https://www.instagram.com/isubhamsr/"
                 rel="noopener noreferrer"
