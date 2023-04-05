92dfec2d4f3c9b750f51f2829007c7ad32d0fe97
isubhamsr <shubhamroy12345@gmail.com>
2023-04-05 21:44:38 +0530

    change in contact page

--- src/modules/core/ContactPage.js
+++ src/modules/core/ContactPage.js
@@ -66,27 +66,9 @@ export default function ContactPage() {
           <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
             Contact Us
           </h1>
-
-          {error ? (
-            <Alert
-              className="lg:w-2/3 mx-auto leading-relaxed text-base"
-              severity="error"
-            >
-              {errorMessage}
-            </Alert>
-          ) : null}
-          {success ? (
-            <Alert
-              className="lg:w-2/3 mx-auto leading-relaxed text-base"
-              severity="success"
-            >
-              Thank You for contact, We will reach soon
-              {/* {message} */}
-            </Alert>
-          ) : null}
         </div>
         <div class="lg:w-1/2 md:w-2/3 mx-auto">
-          <form class="flex flex-wrap -m-2">
+          <form class="flex flex-wrap -m-2" name="contact" data-netlify="true">
             <div class="p-2 w-1/2">
               <div class="relative">
                 <label for="name" class="leading-7 text-sm text-gray-600">
@@ -98,8 +80,6 @@ export default function ContactPage() {
                   name="name"
                   class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   required
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
                 />
               </div>
             </div>
@@ -114,8 +94,6 @@ export default function ContactPage() {
                   name="email"
                   class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                   required
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
                 />
               </div>
             </div>
@@ -129,21 +107,12 @@ export default function ContactPage() {
                   name="message"
                   class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-44 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
                   required
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
                 ></textarea>
               </div>
             </div>
             <div class="p-2 w-full">
-              <button
-                onClick={onSubmit}
-                class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-              >
-                {isSubmit ? (
-                  <CircularProgress color="secondary" />
-                ) : (
-                  "Let's Talk"
-                )}
+              <button type="submit" class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+                  Let's Talk
               </button>
             </div>
           </form>
@@ -151,4 +120,97 @@ export default function ContactPage() {
       </div>
     </section>
   );
+
+  // return (
+  //   <section class="text-gray-600 body-font relative">
+  //     <div class="container px-5 py-24 mx-auto">
+  //       <div class="flex flex-col text-center w-full mb-12">
+  //         <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+  //           Contact Us
+  //         </h1>
+
+  //         {error ? (
+  //           <Alert
+  //             className="lg:w-2/3 mx-auto leading-relaxed text-base"
+  //             severity="error"
+  //           >
+  //             {errorMessage}
+  //           </Alert>
+  //         ) : null}
+  //         {success ? (
+  //           <Alert
+  //             className="lg:w-2/3 mx-auto leading-relaxed text-base"
+  //             severity="success"
+  //           >
+  //             Thank You for contact, We will reach soon
+  //             {/* {message} */}
+  //           </Alert>
+  //         ) : null}
+  //       </div>
+  //       <div class="lg:w-1/2 md:w-2/3 mx-auto">
+  //         <form class="flex flex-wrap -m-2">
+  //           <div class="p-2 w-1/2">
+  //             <div class="relative">
+  //               <label for="name" class="leading-7 text-sm text-gray-600">
+  //                 Name
+  //               </label>
+  //               <input
+  //                 type="text"
+  //                 id="name"
+  //                 name="name"
+  //                 class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+  //                 required
+  //                 value={name}
+  //                 onChange={(e) => setName(e.target.value)}
+  //               />
+  //             </div>
+  //           </div>
+  //           <div class="p-2 w-1/2">
+  //             <div class="relative">
+  //               <label for="email" class="leading-7 text-sm text-gray-600">
+  //                 Email
+  //               </label>
+  //               <input
+  //                 type="email"
+  //                 id="email"
+  //                 name="email"
+  //                 class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+  //                 required
+  //                 value={email}
+  //                 onChange={(e) => setEmail(e.target.value)}
+  //               />
+  //             </div>
+  //           </div>
+  //           <div class="p-2 w-full">
+  //             <div class="relative">
+  //               <label for="message" class="leading-7 text-sm text-gray-600">
+  //                 Message
+  //               </label>
+  //               <textarea
+  //                 id="message"
+  //                 name="message"
+  //                 class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-44 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+  //                 required
+  //                 value={message}
+  //                 onChange={(e) => setMessage(e.target.value)}
+  //               ></textarea>
+  //             </div>
+  //           </div>
+  //           <div class="p-2 w-full">
+  //             <button
+  //               onClick={onSubmit}
+  //               class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+  //             >
+  //               {isSubmit ? (
+  //                 <CircularProgress color="secondary" />
+  //               ) : (
+  //                 "Let's Talk"
+  //               )}
+  //             </button>
+  //           </div>
+  //         </form>
+  //       </div>
+  //     </div>
+  //   </section>
+  // );
 }
